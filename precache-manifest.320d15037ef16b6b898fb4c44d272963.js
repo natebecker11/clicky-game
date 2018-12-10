a149a20e3fdbddafefcd07f783d608e0e9c45dd2
natebecker11 <natebecker11@gmail.com>
2018-12-09 21:43:50 -0500

    Updates

---
+++ precache-manifest.320d15037ef16b6b898fb4c44d272963.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "8eb98373a5f41e46725ff623d8735d03",
+    "url": "/clicky-game/static/media/drogon.8eb98373.jpg"
+  },
+  {
+    "revision": "0942e16a3b3204eda0b8",
+    "url": "/clicky-game/static/css/main.38ad7fac.chunk.css"
+  },
+  {
+    "revision": "1f45ac000c8537521ecf46a38a89d958",
+    "url": "/clicky-game/static/media/tyrion.1f45ac00.jpg"
+  },
+  {
+    "revision": "c886d93a4f8fd1a15d91",
+    "url": "/clicky-game/static/js/1.c886d93a.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "fc928649d5e2b82cde4eb3c54a45fd00",
+    "url": "/clicky-game/static/media/arya.fc928649.jpg"
+  },
+  {
+    "revision": "24915351e48cde1986b6d18ca7725eef",
+    "url": "/clicky-game/static/media/brienne.24915351.jpg"
+  },
+  {
+    "revision": "f5e8e23d62c2aeba6ee111e0e684505a",
+    "url": "/clicky-game/static/media/cersei.f5e8e23d.jpg"
+  },
+  {
+    "revision": "be7f977ececac71df498272cc777d38f",
+    "url": "/clicky-game/static/media/dany.be7f977e.jpg"
+  },
+  {
+    "revision": "0942e16a3b3204eda0b8",
+    "url": "/clicky-game/static/js/main.0942e16a.chunk.js"
+  },
+  {
+    "revision": "22990f27d75a90b99ee05e4d563fb0a7",
+    "url": "/clicky-game/static/media/hound.22990f27.jpg"
+  },
+  {
+    "revision": "f414686141332f5b5e653f9bce0d2481",
+    "url": "/clicky-game/static/media/jaime.f4146861.jpg"
+  },
+  {
+    "revision": "c91d7d4ee56e4e86e7de9654a26aff08",
+    "url": "/clicky-game/static/media/jon.c91d7d4e.jpg"
+  },
+  {
+    "revision": "d4acda4723b348e5d945df757897b47b",
+    "url": "/clicky-game/static/media/mel.d4acda47.jpg"
+  },
+  {
+    "revision": "4cfe736fb2128e42aaa11219f2376f04",
+    "url": "/clicky-game/static/media/ned.4cfe736f.jpg"
+  },
+  {
+    "revision": "1964993208c4f5ec795e1bb50d29b7fd",
+    "url": "/clicky-game/static/media/sansa.19649932.jpg"
+  },
+  {
+    "revision": "c886d93a4f8fd1a15d91",
+    "url": "/clicky-game/static/css/1.200fbb9c.chunk.css"
+  },
+  {
+    "revision": "9936270ea44436a441e483b4c35aba4f",
+    "url": "/clicky-game/index.html"
+  }
+];
